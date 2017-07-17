bbb9e36e3a8b8ff4aad110bb4ddb8c3d33d6d1fd
Parmesh C <parmesh.c@gmail.com>
2017-07-17 16:27:34 +1000

    changed planner getApiResponse from get to post

--- tBOT/Scripts/api-test-planner.js
+++ tBOT/Scripts/api-test-planner.js
@@ -31,33 +31,64 @@ PlannerApp.controller('PlannerCtrl', function (PlannerFactory, apiTestSharedServ
     $scope.apiApp = $scope.AppOptions[0];
 
 
+    //$scope.requestResult = false;
+    //$scope.reqestStatus = false;
+    //$scope.results = [];
+    //$scope.apiRequest = function () {
+    //    $scope.reqestStatus = true;
+    //    $scope.requestResult = false;
+    //    PlannerFactory.getApiResponse($scope).then(function (d) {
+    //        $scope.results = d.data;
+    //        $scope.reqestStatus = false;
+    //        $scope.requestResult = true;
+    //    });
+    //}
+
     $scope.requestResult = false;
     $scope.reqestStatus = false;
     $scope.results = [];
+    $scope.requestData = {};
     $scope.apiRequest = function () {
-        $scope.reqestStatus = true;
-        $scope.requestResult = false;
-        envService.getApiResponse($scope).then(function (d) {
+         $scope.requestData["AuthType"] = $scope.apiAuth.Type;
+        $scope.requestData["UserName"] = $scope.apiAuth.UserName;
+        $scope.requestData["Password"] = $scope.apiAuth.Password;
+        $scope.requestData["Lang"] = "en-in";
+        $scope.requestData["Accept"] = "application/json";
+        $scope.requestData["ContentType"] = "application/json";
+        $scope.requestData["RequestMethod"] = "GET";
+        $scope.requestData["RequestUrl"] = $scope.ApiInfoUrl;
+        $scope.requestData["RequestBody"] = "";
+        $scope.requestData["EndPoint"] = $scope.ApiInfo.EndPoint;
+        $scope.requestData["RawschemaUrl"] = $scope.ApiInfoSchema;
+
+        PlannerFactory.getApiResponse($scope).then(function (d) {
             $scope.results = d.data;
             $scope.reqestStatus = false;
             $scope.requestResult = true;
         });
-    }
+
+    };
+
+
 });
 
 PlannerApp.factory('PlannerFactory', function ($http) {
     return {
 
         getApiResponse: function (scp) {
-            return $http.get("/Planner/GetApiResponse"
-                + "?SrvNm=" + scp.apiEnv.Server                
-                + "&UsrNm=" + scp.apiAuth.UserName
-                + "&PassWrd=" + scp.apiAuth.Password
-                + "&AuthType=" + scp.apiAuth.Type
-                + "&ApiAPP=" + scp.apiApp.app
-                + "&ApiEndPoint=" + scp.apiEndPoint
-                );
+            return $http.post("/Planner/GetApiResponse", scp.requestData);
         },
+
+        //getApiResponse: function (scp) {
+        //    return $http.get("/Planner/GetApiResponse"
+        //        + "?SrvNm=" + scp.apiEnv.Server                
+        //        + "&UsrNm=" + scp.apiAuth.UserName
+        //        + "&PassWrd=" + scp.apiAuth.Password
+        //        + "&AuthType=" + scp.apiAuth.Type
+        //        + "&ApiAPP=" + scp.apiApp.app
+        //        + "&ApiEndPoint=" + scp.apiEndPoint
+        //        );
+        //},
 
         getEnvironments: function () {
             return $http.get("/Planner/GetAllEnvironments");
